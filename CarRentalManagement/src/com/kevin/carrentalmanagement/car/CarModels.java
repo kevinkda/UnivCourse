8395d76b29a36f7621d3b40b5e5f8a5dcd83d758
Kevin KDA <kevin_kda@yahoo.com.au>
2019-09-27 14:03:14 +0800

    Update Project

---
+++ CarRentalManagement/src/com/kevin/carrentalmanagement/car/CarModels.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 Kevin KDA. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.kevin.carrentalmanagement.car;
+
+/**
+ * @Project Java_Course_Assignments
+ * @Package com.kevin.carrentalmanagement.car
+ * @Author Kevin KDA
+ * @Description TODO
+ * @Date 2019/9/27 11:44
+ * @Interface
+ * @Enum
+ * @Version 1.0
+ */
+public class CarModels extends Brand {
+    private int intNumberOfSeats;
+    private String strModel;
+//    private String strNumberPlate;
+
+    public CarModels() {
+    }
+
+    public CarModels(String strSn, String strBrand) {
+        super(strSn, strBrand);
+    }
+}
